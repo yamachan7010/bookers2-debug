c46e059a3f38e82c88adc6eac0f9a00e3f118b42
EC2 Default User <ec2-user@ip-172-31-36-232.ec2.internal>
2023-09-03 12:51:25 +0000

    9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    @this_week_book_counts = []
-    6.downto(0) do |n|
-      @this_week_book_counts.push(@books.where(created_at: n.day.ago.all_day).count)
+  end
+
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
+    get 'search', to: 'users#search'
     resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :chats,only: [:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/search', to: 'searches#search'
+
 end
