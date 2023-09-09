12c5ab528426c3494e94799afa530433ff54c306
EC2 Default User <ec2-user@ip-172-31-36-232.ec2.internal>
2023-09-09 09:44:20 +0000

    8c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,12 @@ class GroupsController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
+
   def new
     @group = Group.new
   end
@@ -21,6 +27,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path, method: :post
     else
@@ -37,6 +44,12 @@ class GroupsController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :chats,only: [:show, :create]
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  resources :groups do
+    get "join" => "groups#join"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
